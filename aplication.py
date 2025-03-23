2d6b9085b1b7adfd28597eaa9c95afe42ec8e9e7
Fabio <fabio.marquezt@gmail.com>
2025-03-22 19:24:13 -0500

    Variables

--- aplication.py
+++ aplication.py
@@ -9,6 +9,15 @@ app = Flask(__name__)
 def index():
     return '<h1>PAGINA DE INICIO</h1>'
 
-@app.route('/aplication/<name>')#/<name> es una variable
-def aplication(name):
-    return f'<h1>Hola, {name}!</h1>'
+
+
+@app.route('/aplication')
+@app.route('/aplication/<name>')
+@app.route('/aplication/<name>/<int:age>')#/<strin:name> es una variable y podemos definir el tipo de dato ene sta caso string
+def aplication(name = None, age= None):
+    if name == None and age == None:
+        return '<h1>Hola Mundo</h1>'
+    elif age == None:
+        return f'<h1>Hola, {name}</h1>'
+    else:
+        return f'<h1>Hola, {name}! y tu edad es {age * 20}</h1>'
