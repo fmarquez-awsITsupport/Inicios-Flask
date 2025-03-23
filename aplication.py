e458e1d48a934cdcd3a2f3d1f6a75f8971fb643f
Fabio <fabio.marquezt@gmail.com>
2025-03-23 12:49:45 -0500

    Render Template HTML

--- aplication.py
+++ aplication.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,17 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return '<h1>PAGINA DE INICIO</h1>'
+    name = 'Fabio'
+    return render_template('index.html', name=name)
 
 
 
 @app.route('/aplication')
 @app.route('/aplication/<name>')
 @app.route('/aplication/<name>/<int:age>')#/<strin:name> es una variable y podemos definir el tipo de dato ene sta caso string
-def aplication(name = None, age= None):
+def aplication(name = None, age= None): # Se asigna un valor nulo para hacer las comparaciones
     if name == None and age == None:
         return '<h1>Hola Mundo</h1>'
     elif age == None:
         return f'<h1>Hola, {name}</h1>'
     else:
         return f'<h1>Hola, {name}! y tu edad es {age * 20}</h1>'
+
+from markupsafe import escape
+@app.route('/code/path:<code>')#Permite ejecutar codigo
+def code(code):
+    return f'<code>{escape(code)}</code>'#escape - sirve para convertir codigo en texto plano por ejemplo un alert de js no se ejecutaria solo seria un texto plano
