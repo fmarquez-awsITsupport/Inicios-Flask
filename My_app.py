601e2ca1320a3d892c0fbbfdb36a212f6b5f7fb5
Fabio <fabio.marquezt@gmail.com>
2025-04-17 12:26:53 -0500

    Validación Formularios lib wtf Final

--- My_app.py
+++ My_app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__) 
+app.config.from_mapping(
+    SECRET_KEY = 'dev'
+)
 
 # Filtros personalizados
 @app.add_template_filter #Funcion decoradora para las plantillas
@@ -51,16 +54,35 @@ from markupsafe import escape
 def code(code):
     return f'<code>{escape(code)}</code>'#escape - sirve para convertir codigo en texto plano por ejemplo un alert de js no se ejecutaria solo seria un texto plano
 
+
+#crear formulario con libreria wtf
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, length
+
+class RegisterForm(FlaskForm):
+    username = StringField("Nombre de usuario: ", validators = [DataRequired(), length(min = 4, max = 25)])
+    password = PasswordField("Password: ", validators = [DataRequired(), length(min = 6, max = 40)])
+    submit = SubmitField("Registrar: ")
+
+
+
 #Registrar Usuario
 @app.route('/auth/register', methods = ['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']#Captutrando el nombre de usuario
-        password = request.form['password']
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        return f"Nombre de usuario: {username}, Contraseña: {password}"
 
-        if len(username) >= 4 and len(username) <= 25 and len(password) >= 6 and len(password) <=40:
-            return f"Nombre de usuario: {username}, Contraseña: {password}"
-        else:
-            error = """Nombre de usuario debe tener caracteres y la contraseña debe tener caracteres"""
-            return render_template('auth/registrer.html', error = error)
-    return render_template('auth/registrer.html')
+    #if request.method == 'POST':
+     #   username = request.form['username']#Captutrando el nombre de usuario
+      #  password = request.form['password']
+
+       # if len(username) >= 4 and len(username) <= 25 and len(password) >= 6 and len(password) <=40:
+        #    return f"Nombre de usuario: {username}, Contraseña: {password}"
+        #else:
+         #   error = """Nombre de usuario debe tener caracteres y la contraseña debe tener caracteres"""
+          #  return render_template('auth/registrer.html', form = form, error = error)
+    return render_template('auth/registrer.html', form = form)
